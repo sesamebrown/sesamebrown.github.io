c22e6f2ecc027dcc5981116503a5f08dd6ce08b4
sesamebrown <142832286+sesamebrown@users.noreply.github.com>
2026-01-28 16:02:37 -0600

    Idle fix

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3e2f3a3eb7aecf96b7f904c4b6400acf60c134574d4b0a09ade732ad8c3c407
-size 4886
+oid sha256:d2464d7d8d4d7d735065e9a2af1391207428ffd748d37d79d0ad53ffe56c7fa1
+size 4877
