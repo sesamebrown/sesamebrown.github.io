f8bfd29a088899df3663788b622e52ea91af60fd
sesamebrown <142832286+sesamebrown@users.noreply.github.com>
2026-02-01 18:51:31 -0600

    Moving HTML elements but not cows yet

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddab1efd8db6246ab01b6a15ea6f67571e04baca89e0ec759daad4b70fff63f2
-size 13201
+oid sha256:e1c310b35811b4194f4799c0f3163f5c2af3e27e35bbf60f1104149245cd1d25
+size 15914
