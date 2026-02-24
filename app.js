f5dac54de5b4840e9d9708723190a6af15d8cb63
sesamebrown <142832286+sesamebrown@users.noreply.github.com>
2026-02-24 10:36:16 -0600

    revamped animations

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c06d398336267df67c298aaa3105d205e263ced0b94fdd01d2d83f15388fa77
-size 14011
+oid sha256:f54e893138bb7d5d477ff60da57f224d405e38a768fd72f73d6b85cc9e3f58f3
+size 8849
---
+++ portfolio.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6522d7de110077534d3e50a3af8aea4cb45615051667d4e4084b8e59a1c8d2db
+size 2927
