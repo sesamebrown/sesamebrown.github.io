eb322df06f89e236017bfb22ec416024eadc7b73
sesamebrown <142832286+sesamebrown@users.noreply.github.com>
2026-05-23 11:57:59 -0500

    Refactoring

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.129.0/build/three.m
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.6.1/index.js';
 
+// scene
+const scene = new THREE.Scene();
+let ufo;
+let mixer;
+
+// camera
 const camera = new THREE.PerspectiveCamera(
     10,
     window.innerWidth / window.innerHeight,
@@ -13,278 +19,130 @@ camera.position.z = 50;
 camera.position.y = 5;
 camera.rotateX(-0.1);
 
-const scene = new THREE.Scene();
-let UFO;
-let mixer;
-let UFOscene;
-let UFOpositions;
-let ufoIdleMoveTween;
-let ufoIdleRotateTween;
-
-const loader = new GLTFLoader();
-loader.load('/glb/UFOscene.glb',
-    function (gltf) {
-        UFOscene = gltf.scene;
-        // UFOscene.rotation.set(0, Math.PI/5, 0);
-        // UFO = gltf.scene;
-        
-
-        UFOscene.position.set(0, -3, 0);
-
-        UFO = UFOscene.getObjectByName('UFO_Root');
-        UFO.position.set(-3, 3, -5);
-        UFO.rotation.set(0.1, 0.15, 0.1);
-        const dome = UFOscene.getObjectByName('dome001')
-        const beam = UFOscene.getObjectByName('Beam')
-        const body = UFOscene.getObjectByName('UFO001')
-        // idle
-        // UFO.position.set(-2.5, 0.3, -5);
-        // UFO.rotation.set(0.1, 0.15, 0.1);
-
-        // about
-        // UFO.position.set(-5.3, 3.5, 2);
-        // UFO.rotation.set(0.05, 0.3, 0.15);
-
-        //portfolio
-        // UFO.position.set(-1.5, -0.2, 0);
-        // UFO.rotation.set(-0.1, -0.2, -0.1);
-
-        //qualifications z =
-        // UFO.position.set(0.65, 0.5, -1);
-        // UFO.rotation.set(0.15, 0.1, 0.2);
-
-        //service
-        // UFO.position.set(5.5, 0.2, 1);
-        // UFO.rotation.set(-0.25, -0.5, -0.25);
-        beam.visible = false;
-
-        dome.material.transparent = true
-        dome.material.opacity = 0.3
-        
-        beam.material.transparent = true
-        beam.material.opacity = 0.5
-        beam.material.depthWrite = false
-        beam.material.emissive.set(0x00ffff)
-        beam.material.emissiveIntensity = 2
-
-        gsap.to([dome.rotation, body.rotation], {
-            y: `+=${Math.PI * 2}`,
-            duration: 2.5,
-            ease: "none",
-            repeat: -1
-        });
-        startUFOIdleMotion();
-
-        // positions
-        UFOpositions = [
-            {id: 'idle', position: {x: -3, y: 3, z: -5}, rotation: {x: 0.1, y: 0.15, z: 0.1}},
-            {id: 'cow-about', position: UFOscene.getObjectByName('position_1').position, rotation: UFOscene.getObjectByName('position_1').rotation},
-            {id: 'cow-portfolio', position: UFOscene.getObjectByName('position_2').position, rotation: UFOscene.getObjectByName('position_2').rotation},
-            {id: 'cow-qualifications', position: UFOscene.getObjectByName('position_3').position, rotation: UFOscene.getObjectByName('position_3').rotation},
-            {id: 'cow-service', position: UFOscene.getObjectByName('position_4').position, rotation: UFOscene.getObjectByName('position_4').rotation}
-        ]
-        
-        scene.add(UFOscene);
-
-        mixer = new THREE.AnimationMixer(UFOscene);
-        mixer.timeScale = 0.5;
-
-        modelMove();
-    },
-function (xhr) {},
-function (error) {
-    console.error("GLTF Load Error:", error);
-}
-);
-
-
-let cowPositionModel = [
-    {
-        id: 'cow-about',
-        position: {x: -4.8, y: -1.75, z: 1.5},
-        rotation: {x: 0, y: 1, z: 0},
-        scale: {x: 0.5, y: 0.5, z: 0.5}
-    },
-    {
-        id: 'cow-portfolio',
-        position: {x: -1.75, y: -2.5, z: 0},
-        rotation: {x: 0, y: -0.75, z: 0},
-        scale: {x: 0.5, y: 0.5, z: 0.5}
-    },
-    {
-        id: 'cow-qualifications',
-        position: {x: 1.1, y: -1.75, z: -1.5},
-        rotation: {x: 0, y: 0.5, z:0},
-        scale: {x: 0.5, y: 0.5, z:0.5}
-    },
-    {
-        id: 'cow-service',
-        position: {x: 5, y: -2.15, z: 1},
-        rotation: {x: 0, y: -1.3, z: 0},
-        scale: {x: 0.5, y: 0.5, z: 0.5}
-    }
-];
-
+// renderer
 const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container3D').appendChild(renderer.domElement);
+const keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false
+};
 
-// light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
-const topLight = new THREE.DirectionalLight(0xffffff, 0.2);
-topLight.position.set(500, 500, 500);
-scene.add(topLight);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const reRender3D = () => {
-    requestAnimationFrame(reRender3D);
+// loader
+const loader = new GLTFLoader();
+loader.load('/glb/ufo.glb',
+    function (gltf) {
+        ufo = gltf.scene;
+        scene.add(ufo);
+
+        mixer = new THREE.AnimationMixer(UFOscene);
+            mixer.timeScale = 0.5;
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+            console.error("GLTF Load Error:", error);
+    }
+);
+
+// keyboard input
+window.addEventListener('keydown', (e) => {
+    const key = e.key.toLowerCase();
+
+    if (keys.hasOwnProperty(key)) {
+        keys[key] = true;
+        lastInputTime = Date.now();
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    const key = e.key.toLowerCase();
+
+    if (keys.hasOwnProperty(key)) {
+        keys[key] = false;
+    }
+});
+
+// mouse input
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+window.addEventListener('mousemove', (event) => {
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+
+function checkHover() {
+
+    if (!ufo) return;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObject(ufo, true);
+
+    if (intersects.length > 0) {
+
+        // Mouse is hovering UFO
+
+        console.log("hovering!");
+
+        ufo.rotation.y += 0.02;
+
+    }
+}
+
+// animation
+const animate = () => {
+    requestAnimationFrame(animate);
+    updateMovement();
+    checkHover();
     renderer.render(scene, camera);
     if (mixer) mixer.update(0.02);
 };
-reRender3D();
+animate();
 
-function stopUFOIdleMotion() {
-    if (ufoIdleMoveTween) {
-        ufoIdleMoveTween.kill();
-        ufoIdleMoveTween = null;
+const speed = 0.1;
+
+function updateMovement() {
+    if (!ufo) return;
+    let moving = false;
+
+    if (keys.w) {
+        ufo.position.z -= speed;
+        moving = true;
     }
 
-    if (ufoIdleRotateTween) {
-        ufoIdleRotateTween.kill();
-        ufoIdleRotateTween = null;
+    if (keys.s) {
+        ufo.position.z += speed;
+        moving = true;
+    }
+
+    if (keys.a) {
+        ufo.position.x -= speed;
+        moving = true;
+    }
+
+    if (keys.d) {
+        ufo.position.x += speed;
+        moving = true;
+    }
+
+    // If idle for 3 seconds
+    if (Date.now() - lastInputTime > 3000) {
+        ufo.position.lerp(idlePosition, 0.02);
     }
 }
 
-function startUFOIdleMotion() {
-    if (!UFO) return;
-
-    stopUFOIdleMotion();
-
-    ufoIdleMoveTween = gsap.to(UFO.position, {
-        y: `+=0.4`,
-        duration: 1.5,
-        ease: "sine.inOut",
-        yoyo: true,
-        repeat: -1
-    });
-}
-
-function moveUFOTo(newCoordinates, onComplete) {
-    if (!UFO || !newCoordinates) return;
-
-    gsap.killTweensOf(UFO.position);
-    gsap.killTweensOf(UFO.rotation);
-
-    const moveTimeline = gsap.timeline({
-        onComplete: () => {
-            if (onComplete) onComplete();
-        }
-    });
-
-    moveTimeline.to(UFO.position, {
-        x: newCoordinates.position.x,
-        y: newCoordinates.position.y,
-        z: newCoordinates.position.z,
-        duration: 2,
-        ease: "power2.out"
-    }, 0);
-
-    moveTimeline.to(UFO.rotation, {
-        x: newCoordinates.rotation.x,
-        y: newCoordinates.rotation.y,
-        z: newCoordinates.rotation.z,
-        duration: 2,
-        ease: "power3.out"
-    }, 0);
-}
-
-const modelMove = () => {
-    let idleTimeout = null;
 
 
-    const cowsHTML = document.querySelectorAll('.menu-cows');
-    const beam = UFOscene.getObjectByName('Beam')
-
-    cowsHTML.forEach((cowHTML) => {
-        cowHTML.addEventListener('mouseenter', () => {
-            clearTimeout(idleTimeout);
-            stopUFOIdleMotion();
-
-            let position_active = UFOpositions.findIndex(
-                (val) => val.id == cowHTML.id
-            );
-
-            if (position_active >= 0) {
-                let new_coordinates = UFOpositions[position_active];
-                moveUFOTo(new_coordinates, () => {
-                    startUFOIdleMotion();
-                });
-
-                // beam animation
-                beam.scale.set(0.3, 0, 0.3);
-                beam.visible = true;
-                gsap.to(beam.scale, {
-                    x: 1,
-                    y: 1,
-                    z: 1,
-                    delay: 1,
-                    duration: 2,
-                    ease: "power2.out"
-                });
-
-                // cow animation
-                const cowModel = UFOscene.getObjectByName(`cow${position_active}`);
-                if (cowModel) {
-                    const t = 1; // percnet of the way to the target position
-                    console.log(UFOpositions[position_active].id);
-                    gsap.to(cowModel.position, {
-                        x: gsap.utils.interpolate(cowModel.position.x, UFOpositions[position_active].position.x, t),
-                        y: gsap.utils.interpolate(cowModel.position.y, UFOpositions[position_active].position.y, t),
-                        z: gsap.utils.interpolate(cowModel.position.z, UFOpositions[position_active].position.z, t),
-                        duration: 1,
-                        ease: "power2.out"
-                    });
-                }
-                else {
-                    console.warn(`Cow model for position ${position_active} not found.`);
-                }
-            }
-        })
-
-        cowHTML.addEventListener('mouseleave', () => {
-            idleTimeout = setTimeout(() => {
-                let new_coordinates = UFOpositions[0];
-
-                moveUFOTo(new_coordinates, () => {
-                    startUFOIdleMotion();
-                });
-
-                gsap.to(beam.scale, {
-                    x: .3,
-                    y: 0,
-                    z: .3,
-                    duration: 2,
-                    ease: "power2.out"
-                });
-
-                setTimeout(() => {
-                    gsap.to(beam.scale, {
-                        x: 0.3,
-                        y: 0,
-                        z: 0.3,
-                        duration: 1,
-                        ease: "power2.out"
-                    });
-                    beam.visible = false;
-                }, 1500);
-
-            }, 1500); 
-        });
-    })
-};
-
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-});
+// ufo stuff
+const idlePosition = new THREE.Vector3(0, 0, 0);
+let lastInputTime = Date.now();
